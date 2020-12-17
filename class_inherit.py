f3973a53f9a14c049a20c4bd9f9f6de230d8b625
rmccabe62 <rmccabe1023@gmail.com>
2020-12-17 16:40:27 -0500

    class inheritance file

---
+++ class_inherit.py
@@ -0,0 +1,19 @@
+
+
+
+class Student:
+    studentName = 'No name provided'
+    email = ' '
+    password = 'password1'
+    major = ' '
+
+class Teacher(Student):
+    subject = 'Math'
+    semester = 'Spring'
+
+class GPA(Student):
+    testAvg = 'B'
+    attendance = 'Satisfactory'
+
+    
+    
