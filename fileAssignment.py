322f98cfcf78bca4196728a1f9d9e394d9b2cc6b
rmccabe62 <rmccabe1023@gmail.com>
2020-12-17 13:43:48 -0500

    updated file

--- fileAssignment.py
+++ fileAssignment.py
@@ -1,9 +1,9 @@
 
-
+#import sqlite3 to work with a database
 import sqlite3
-
+# connect to database using sqlite3
 conn = sqlite3.connect('test.db')
-
+# create a table with fields
 with conn:
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_fileList( \
@@ -14,12 +14,15 @@ with conn:
 conn.close()
 
 conn = sqlite3.connect('test.db')
+# create a variable list to pull from
 fileList = ('information.docx','Hello.txt','myImage.png', \
             'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
+# for loop including if conditional statement to query text files from list
 for x in fileList:
     if x.endswith('.txt'):
         with conn:
             cur = conn.cursor()
+            # insert values into table to query
             cur.execute("INSERT INTO tbl_fileList(col_name) VALUES (?)",(x,))
             cur.execute("INSERT INTO tbl_fileList(col_name) VALUES (?)",(x,))
         print(x)
