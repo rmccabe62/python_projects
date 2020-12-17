a191097cfea38fcfd60b6bf5e9e4629103587b06
rmccabe62 <rmccabe1023@gmail.com>
2020-12-17 16:43:23 -0500

    updated files

--- class_inherit.py
+++ class_inherit.py
@@ -1,16 +1,18 @@
 
 
-
+# create parent class
 class Student:
     studentName = 'No name provided'
     email = ' '
     password = 'password1'
     major = ' '
-
+    
+# create first child class for parent
 class Teacher(Student):
     subject = 'Math'
     semester = 'Spring'
 
+# create second child class for parent
 class GPA(Student):
     testAvg = 'B'
     attendance = 'Satisfactory'
