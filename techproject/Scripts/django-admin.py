124a3879f6dc464cc4f833b3dfee3cf8afc14e28
rmccabe62 <72869399+rmccabe62@users.noreply.github.com>
2021-01-16 15:58:15 -0500

    update

---
+++ techproject/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\rmcca\OneDrive\Documents\GitHub\python_projects\django_projects\techproject\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
