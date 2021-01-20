2105e5772b05aeb5106cadb57ae82bb492d0af26
rmccabe62 <72869399+rmccabe62@users.noreply.github.com>
2021-01-19 19:33:25 -0500

    updated files

--- techproject/src/mainapp/products/views.py
+++ techproject/src/mainapp/products/views.py
@@ -30,5 +30,28 @@ def delete(request, pk):
     if request.method == 'POST':
         item.delete()
         return redirect('admin_console')
-    context = ("item": item,)
+    context = {"item": item,}
     return render(request, "products/confirmDelete.html", context)
+
+def confirmed(request):
+    if request.method == 'POST':
+        # creates an instance and binds data to it.
+        form = ProductForm(request.POST or None)
+        if form.is_valid():
+            form.delete()
+            return redirect('admin_console')
+    else:
+        return redirect('admin_console')
+
+def createRecord(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('admin_console')
+    else:
+        print(form.errors)
+        form = ProductForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'products/createRecord.html', context)
